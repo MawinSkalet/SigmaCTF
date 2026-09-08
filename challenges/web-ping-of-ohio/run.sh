e972c0b01e9c3dfd0851a832977191acbc7f4071
Jirasak <jeerasak82560@gmail.com>
2026-09-08 13:52:54 +0700

    fix(challenge): set APACHE_LOG_DIR=/tmp for read-only rootfs in ping-of-ohio

--- challenges/web-ping-of-ohio/run.sh
+++ challenges/web-ping-of-ohio/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eu
+export APACHE_LOG_DIR=/tmp
 remaining=$((EXPIRES_EPOCH - $(date +%s)))
 [ "$remaining" -gt 0 ] || exit 0
 exec timeout --signal=TERM --kill-after=1 "$remaining" apache2 -D FOREGROUND
